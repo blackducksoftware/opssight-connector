cc328db8d535394bdeccb63a75529db324200592
Senthil Murugan <msenmurugan@gmail.com>
2018-04-05 11:54:02 -0400

    Modified the way to get the private docker registry, so that it will for OpenShift 3.9

--- install/common/oadm-policy-init.sh
+++ install/common/oadm-policy-init.sh
@@ -27,7 +27,7 @@ oc policy $ROLE view system:serviceaccount::perceptor-scanner-sa
 _arg_private_registry_token=$(oc sa get-token perceptor-scanner-sa)
 
 # Get the default Docker Registry
-route_docker_registry=$(oc get route -n default | egrep "^docker-registry[[:space:]].+$" | tr -s ' ' | cut -d ' ' -f 2)
-service_docker_registry=$(oc get svc -n default | egrep "^docker-registry[[:space:]].+$" | tr -s ' ' | cut -d ' ' -f 2)
-service_docker_registry_port=$(oc get svc -n default | egrep "^docker-registry[[:space:]].+$" | tr -s ' ' | cut -d ' ' -f 4 | cut -d '/' -f 1)
+route_docker_registry=$(oc get route docker-registry -o jsonpath='{.spec.host}')
+service_docker_registry=$(oc get svc docker-registry -o jsonpath='{.spec.clusterIP}')
+service_docker_registry_port=$(oc get svc docker-registry -o jsonpath='{.spec.ports[0].port}')
 _arg_private_registry+=("docker-registry.default.svc:5000" $route_docker_registry "$route_docker_registry:443" $service_docker_registry:$service_docker_registry_port)
