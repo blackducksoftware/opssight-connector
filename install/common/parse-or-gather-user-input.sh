40a6eb3a8d5dce42b2bb85d8c97af97ce8e7a850
Senthil Murugan Manikantan <msenmurugan@gmail.com>
2018-04-23 10:21:11 -0400

    Changed the default port number from 8443 to 443

--- install/common/parse-or-gather-user-input.sh
+++ install/common/parse-or-gather-user-input.sh
@@ -10,7 +10,7 @@ function prompt() {
     echo "Interactive"
     echo "============================================"
     read -p "Hub server host (e.g. hub.mydomain.com): " _arg_hub_host
-    read -p "Hub server port [8443]: " _arg_hub_port
+    read -p "Hub server port [443]: " _arg_hub_port
     read -p "Hub user name [sysadmin]: " _arg_hub_user
     read -sp "Hub user password : " _arg_hub_password
     echo " "
