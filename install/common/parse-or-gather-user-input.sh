a4b465dfa95bc9be00ae03824351ea68054c6f74
Senthil Murugan Manikantan <msenmurugan@gmail.com>
2018-04-24 16:05:04 -0400

    fix-interactive-input

--- install/common/parse-or-gather-user-input.sh
+++ install/common/parse-or-gather-user-input.sh
@@ -9,16 +9,21 @@ function prompt() {
     echo "============================================"
     echo "Interactive"
     echo "============================================"
-    read -p "Hub server host (e.g. hub.mydomain.com): " _arg_hub_host
-    read -p "Hub server port [443]: " _arg_hub_port
-    read -p "Hub user name [sysadmin]: " _arg_hub_user
+    read -p "Hub server host (e.g. hub.mydomain.com): " hub_host
+    read -p "Hub server port [443]: " hub_port
+    read -p "Hub user name [sysadmin]: " hub_user
     read -sp "Hub user password : " _arg_hub_password
     echo " "
-    read -p "Maximum concurrent scans [7]: " _arg_hub_max_concurrent_scans
+    read -p "Maximum concurrent scans [7]: " hub_max_concurrent_scans
     echo "============================================"
   else
-    echo "Skipping prompts, --proto_prompty was turned off."
+    echo "Skipping prompts, --prompt was turned off."
   fi
 }
 
 prompt
+
+_arg_hub_host=${hub_host:-$_arg_hub_host}
+_arg_hub_port=${hub_port:-$_arg_hub_port}
+_arg_hub_user=${hub_user:-$_arg_hub_user}
+_arg_hub_max_concurrent_scans=${hub_max_concurrent_scans:-$_arg_hub_max_concurrent_scans}
